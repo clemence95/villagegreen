66e1b9f2d12a4a68f3a2b5a0a0e1a79cf4736379
clemence95 <boitelleclemence@outlook.fr>
2024-06-13 09:33:35 +0200

    .

--- src/Controller/DBALController.php
+++ src/Controller/DBALController.php
@@ -1,38 +1,72 @@
 <?php
 
-use Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Application;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+namespace App\Controller;
 
-require 'vendor/autoload.php';
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-$kernel = new \App\Kernel('dev', true);
-$kernel->boot();
-$entityManager = $kernel->getContainer()->get(EntityManagerInterface::class);
+class DBALController extends AbstractController
+{
+    private $connection;
 
-$cli = new Application('Doctrine Command Line Interface');
-$cli->setCatchExceptions(true);
-$cli->setHelperSet(new \Symfony\Component\Console\Helper\HelperSet([
-    'em' => new EntityManagerHelper($entityManager)
-]));
-$cli->add(new ConvertMappingCommand());
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
 
-$output = new ConsoleOutput();
-$input = new ArrayInput([
-    'command' => 'orm:convert-mapping',
-    '--namespace' => 'App\\Entity\\',
-    '--force' => true,
-    'to-type' => 'annotation',
-    'dest-path' => 'src/Entity'
-]);
+    #[Route('/generate-entities', name: 'generate_entities')]
+    public function generateEntities(): Response
+    {
+        $schemaManager = $this->connection->createSchemaManager();
+        $tables = $schemaManager->listTables();
 
-$cli->run($input, $output);
+        foreach ($tables as $table) {
+            $tableName = $table->getName();
+            $columns = $table->getColumns();
 
+            $entityCode = "<?php\n\nnamespace App\Entity;\n\nuse Doctrine\ORM\Mapping as ORM;\n\n/**\n * @ORM\Entity\n * @ORM\Table(name=\"$tableName\")\n */\nclass " . ucfirst($tableName) . "\n{\n";
 
+            foreach ($columns as $column) {
+                $fieldName = $column->getName();
+                $fieldType = $this->mapColumnType($column->getType()->getName());
 
+                $entityCode .= "    /**\n     * @ORM\Column(type=\"$fieldType\")\n     */\n";
+                $entityCode .= "    private $" . $fieldName . ";\n\n";
+
+                // Generate getters and setters
+                $camelCaseFieldName = ucfirst($fieldName);
+                $entityCode .= "    public function get$camelCaseFieldName(): $fieldType\n    {\n        return \$this->$fieldName;\n    }\n\n";
+                $entityCode .= "    public function set$camelCaseFieldName($fieldType \$$fieldName): self\n    {\n        \$this->$fieldName = \$$fieldName;\n        return \$this;\n    }\n\n";
+            }
+
+            $entityCode .= "}\n";
+
+            $filePath = sprintf('../src/Entity/%s.php', ucfirst($tableName));
+            file_put_contents($filePath, $entityCode);
+        }
+
+        return new Response('Entities generated successfully.');
+    }
+
+    private function mapColumnType(string $type): string
+    {
+        $typeMap = [
+            Types::INTEGER => 'int',
+            Types::STRING => 'string',
+            Types::TEXT => 'string',
+            Types::DATETIME_MUTABLE => '\DateTime',
+            Types::DATE_MUTABLE => '\DateTime',
+            Types::DECIMAL => 'float',
+            Types::FLOAT => 'float',
+            // Add more mappings as needed
+        ];
+
+        return $typeMap[$type] ?? 'string';
+    }
+}
 
 
 
