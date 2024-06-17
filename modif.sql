a1e3c2994e86dc5f5d2ff3a676f2e93a1496bf40
clemence95 <boitelleclemence@outlook.fr>
2024-06-17 08:32:26 +0200

    .

--- modif.sql
+++ modif.sql
@@ -1,9 +1,9 @@
 -- Utiliser la base de données VillageGreen
 USE VillageGreen;
 
-DROP DATABASE villageGreen;
+DROP DATABASE VillageGreen;
 
-CREATE DATABASE villagegreen;
+CREATE DATABASE VillageGreen;
 
 -- Modifier la colonne Photo dans souscategorie de LONGBLOB à VARCHAR(255)
 ALTER TABLE souscategorie MODIFY COLUMN Photo VARCHAR(255);
