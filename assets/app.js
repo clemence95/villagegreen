2dd21405b725cfd64bed31d49f9c8eefc86b5df9
clemence95 <boitelleclemence@outlook.fr>
2024-06-25 16:12:40 +0200

    .

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import './bootstrap.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';  // Note the relative path './' instead of '/assets/'
 
