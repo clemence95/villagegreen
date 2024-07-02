fd0116322a8d85a7718150cbf03464cfb8680ed9
boitelle clémence <boitelleclemence@outlook.fr>
2024-07-02 02:50:54 +0200

    .

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,12 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';  // Note the relative path './' instead of '/assets/'
 
-// Import Bootstrap CSS and JS
-import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap CSS
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JS
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+
+
 
