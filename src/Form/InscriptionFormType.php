ea89283621721995208b1dacbe20459ddf98f0ce
clemence95 <boitelleclemence@outlook.fr>
2024-07-11 16:32:27 +0200

    .

--- src/Form/InscriptionFormType.php
+++ src/Form/InscriptionFormType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
