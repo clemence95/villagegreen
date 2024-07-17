b9c4747909d1ad4d51da408df3d994a8e0f261a4
clemence95 <boitelleclemence@outlook.fr>
2024-07-17 11:08:16 +0200

    .

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -1,31 +1,20 @@
 <?php
-
 // src/Controller/ProfilController.php
 
 namespace App\Controller;
 
-use App\Entity\Commande;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'profil')]
-    public function profil(EntityManagerInterface $entityManager): Response
+    #[Route('/profil', name: 'app_profil')]
+    public function index(): Response
     {
-        $user = $this->getUser();
-        if (!$user) {
-            return $this->redirectToRoute('app_login');
-        }
-
-        // Récupérer les commandes de l'utilisateur
-        $commandes = $entityManager->getRepository(Commande::class)->findBy(['client' => $user]);
-
+        // Rend la vue Twig 'profil/index.html.twig'
         return $this->render('profil/index.html.twig', [
-            'user' => $user,
-            'commandes' => $commandes,
+            'user' => $this->getUser(),  // Récupère l'utilisateur connecté
         ]);
     }
 }
