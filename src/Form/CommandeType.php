f85387580d0d142b525e0193eb9e52559ebd61da
clemence95 <boitelleclemence@outlook.fr>
2024-07-23 14:57:26 +0200

    .

--- src/Form/CommandeType.php
+++ src/Form/CommandeType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CommandeType extends AbstractType
 {
