37371311973359f0e9ff5f8eed4af50be0bfe26d
clemence95 <boitelleclemence@outlook.fr>
2024-08-01 10:48:34 +0200

    .

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\ProduitRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
+#[ApiResource]
 class Produit
 {
     #[ORM\Id]
