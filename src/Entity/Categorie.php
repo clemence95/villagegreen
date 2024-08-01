04595e0f25424ce0595cb3876b1445873108b0aa
clemence95 <boitelleclemence@outlook.fr>
2024-08-01 14:13:51 +0200

    .

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
