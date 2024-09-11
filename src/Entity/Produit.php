48fcc0bc77af246862bb4f32cc6d769577d0d2ec
clemence95 <boitelleclemence@outlook.fr>
2024-09-11 15:33:56 +0200

    .

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -3,28 +3,32 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProduitRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Serializer\Attribute\Groups;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
 #[ApiResource(
-    normalizationContext: ['groups' => ['produit:read']],
+    normalizationContext: ['groups' => ['produit:read_minimal']],
     denormalizationContext: ['groups' => ['produit:write']],
     security: "is_granted('ROLE_ADMIN')"
 )]
+#[ApiFilter(SearchFilter::class, properties: ['sousCategorie' => 'exact'])]
 class Produit
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    #[Groups(['produit:read', 'categorie:read'])]
+    #[Groups(['produit:read_minimal', 'produit:read', 'categorie:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['produit:read', 'produit:write', 'categorie:read'])]
+    #[Groups(['produit:read_minimal', 'produit:write', 'categorie:read'])]
     private ?string $libelleCourt = null;
 
     #[ORM\Column(length: 255)]
@@ -51,29 +55,34 @@ class Produit
     #[Groups(['produit:read', 'produit:write'])]
     private ?bool $actif = null;
 
-    #[ORM\ManyToOne(targetEntity: Categorie::class, inversedBy: 'produits')]
+    #[ORM\ManyToOne(targetEntity: Categorie::class, inversedBy: 'produits', fetch: 'LAZY')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['produit:read', 'produit:write'])]
+    #[Groups(['produit:read_minimal', 'produit:write'])]
+    #[MaxDepth(1)]
     private ?Categorie $sousCategorie = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(['produit:read', 'produit:write'])]
     private ?string $photo = null;
 
-    #[ORM\ManyToOne(targetEntity: Fournisseur::class)]
+    #[ORM\ManyToOne(targetEntity: Fournisseur::class, fetch: 'LAZY')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['produit:read', 'produit:write'])]
+    #[MaxDepth(1)]
     private ?Fournisseur $idFournisseur = null;
 
-    #[ORM\OneToMany(mappedBy: 'produit', targetEntity: CommandeProduit::class)]
+    #[ORM\OneToMany(mappedBy: 'produit', targetEntity: CommandeProduit::class, fetch: 'LAZY')]
     #[Groups(['produit:read'])]
+    #[MaxDepth(1)]
     private Collection $commandeProduits;
 
+    // Constructeur pour initialiser les collections
     public function __construct()
     {
         $this->commandeProduits = new ArrayCollection();
     }
 
+    // Getters et Setters
     public function getId(): ?int
     {
         return $this->id;
@@ -87,6 +96,7 @@ class Produit
     public function setLibelleCourt(string $libelleCourt): self
     {
         $this->libelleCourt = $libelleCourt;
+
         return $this;
     }
 
@@ -98,6 +108,7 @@ class Produit
     public function setLibelleLong(string $libelleLong): self
     {
         $this->libelleLong = $libelleLong;
+
         return $this;
     }
 
@@ -109,6 +120,7 @@ class Produit
     public function setReferenceFournisseur(string $referenceFournisseur): self
     {
         $this->referenceFournisseur = $referenceFournisseur;
+
         return $this;
     }
 
@@ -120,6 +132,7 @@ class Produit
     public function setPrixAchat(float $prixAchat): self
     {
         $this->prixAchat = $prixAchat;
+
         return $this;
     }
 
@@ -131,6 +144,7 @@ class Produit
     public function setPrixVente(float $prixVente): self
     {
         $this->prixVente = $prixVente;
+
         return $this;
     }
 
@@ -142,10 +156,11 @@ class Produit
     public function setStock(int $stock): self
     {
         $this->stock = $stock;
+
         return $this;
     }
 
-    public function isActif(): ?bool
+    public function getActif(): ?bool
     {
         return $this->actif;
     }
@@ -153,6 +168,7 @@ class Produit
     public function setActif(bool $actif): self
     {
         $this->actif = $actif;
+
         return $this;
     }
 
@@ -164,6 +180,7 @@ class Produit
     public function setSousCategorie(?Categorie $sousCategorie): self
     {
         $this->sousCategorie = $sousCategorie;
+
         return $this;
     }
 
@@ -175,6 +192,7 @@ class Produit
     public function setPhoto(string $photo): self
     {
         $this->photo = $photo;
+
         return $this;
     }
 
@@ -186,12 +204,10 @@ class Produit
     public function setIdFournisseur(?Fournisseur $idFournisseur): self
     {
         $this->idFournisseur = $idFournisseur;
+
         return $this;
     }
 
-    /**
-     * @return Collection<int, CommandeProduit>
-     */
     public function getCommandeProduits(): Collection
     {
         return $this->commandeProduits;
@@ -210,7 +226,6 @@ class Produit
     public function removeCommandeProduit(CommandeProduit $commandeProduit): self
     {
         if ($this->commandeProduits->removeElement($commandeProduit)) {
-            // set the owning side to null (unless already changed)
             if ($commandeProduit->getProduit() === $this) {
                 $commandeProduit->setProduit(null);
             }
@@ -219,6 +234,7 @@ class Produit
         return $this;
     }
 }
+
 
 
 
